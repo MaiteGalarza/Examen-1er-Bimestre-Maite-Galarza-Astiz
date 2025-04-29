40c87b705735c15fd7f82c65410317d6d3981b70
MaiteGalarza <49189070@est.ort.edu.ar>
2025-04-29 08:25:12 -0300

    mensaje de error

---
+++ Assets/Scripts/PersonalTrainer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonalTrainer : MonoBehaviour
+{
+    public string tipoDeServicio;
+    public 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
